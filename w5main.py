638d9125ddab7b365fc8cc2b5f544a3f471cb214
JeonHeeSang <gmltkd1302@naver.com>
2016-04-03 18:39:43 +0900

    가위바위보

--- w5main.py
+++ w5main.py
@@ -1,23 +1,24 @@
-﻿﻿marks=raw_input("input your Score(0~100):")
-marks=float(marksTmp)
-print "your Score is:", marksTmp
-
-def convertScoretoGrade(marks):
-    if 90<=float(marks)<=100:
-        grade="A"
-    if 80<=float(marks)<90:
-        grade="B"
-    if 70<=float(marks)<80:
-        grade="C"
-    if 60<=float(marks)<70:
-        grade="D"
-    if  float(marks)<60:
-        print "Congratulations!! you are F Grade!!"
-        grade="F"
-    else:
-        print "Score's can not be higher than 100 or lower than 0"
-        return grade
-
-result=convertScoretoGrade(marks)
-print "your grade is %s" %result
-        
+﻿def game():
+    print "Let's play R S P Game"
+    UA=raw_input("A:")
+    UB=raw_input("B:")
+    def N(U):
+        if(U=="R"):
+            return 1.0
+        elif(U=="S"):
+            return 2.0
+        elif(U=="P"):
+            return 4.0
+        else:
+            print "Input Error"
+    p=N(UA)/N(UB)
+    if(p==1.0):
+        print "draws"
+    elif(p==0.5):
+        print "A wins"
+    elif(p==0.25):
+        print "B wins"
+    elif(p==2):
+        print "B wins"
+    elif(p==4):
+        print "A wins"
