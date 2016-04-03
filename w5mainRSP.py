4ca3d44c39358065d36f76f5ee628e0c12561374
p2_201611103 전희상 <gmltkd1302@naver.com>
2016-04-03 19:42:52 +0900

    이게가위바위보과제입니다.

---
+++ w5mainRSP.py
@@ -0,0 +1,24 @@
+﻿def game():
+    print "Let's play R S P Game"
+    UA=raw_input("A:")
+    UB=raw_input("B:")
+    def N(U):
+        if(U=="R"):
+            return 1.0
+        elif(U=="S"):
+            return 2.0
+        elif(U=="P"):
+            return 4.0
+        else:
+            print "Input Error"
+    p=N(UA)/N(UB)
+    if(p==1.0):
+        print "draws"
+    elif(p==0.5):
+        print "A wins"
+    elif(p==0.25):
+        print "B wins"
+    elif(p==2):
+        print "B wins"
+    elif(p==4):
+        print "A wins"
