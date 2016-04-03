58f512f057cce026a479966b9a23c84fc24406ee
JeonHeeSang <gmltkd1302@naver.com>
2016-04-03 18:42:32 +0900

    Added files via upload

---
+++ w5main().py
@@ -0,0 +1,22 @@
+﻿marks=raw_input("input your Score(0~100):")
+marks=float(marksTmp)
+print "your Score is:", marksTmp
+
+def convertScoretoGrade(marks):
+    if 90<=float(marks)<=100:
+        grade="A"
+    if 80<=float(marks)<90:
+        grade="B"
+    if 70<=float(marks)<80:
+        grade="C"
+    if 60<=float(marks)<70:
+        grade="D"
+    if  float(marks)<60:
+        print "Congratulations!! you are F Grade!!"
+        grade="F"
+    else:
+        print "Score's can not be higher than 100 or lower than 0"
+        return grade
+
+result=convertScoretoGrade(marks)
+print "your grade is %s" %result
