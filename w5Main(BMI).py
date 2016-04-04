bd9842fb964635df6b51207e5433fdd7adc2ab76
p2_201611103 전희상 <gmltkd1302@naver.com>
2016-04-04 17:30:11 +0900

    Added files via upload

---
+++ w5Main(BMI).py
@@ -0,0 +1,17 @@
+﻿def BMI():
+    height=input ("input user height (m):")
+    weight=input ("input user weight (kg):")
+    BMI=weight/(height*height)
+    print BMI
+    if BMI<=18.5:
+        res= 'underweight'
+    elif 18.5<BMI<=23:
+        res= 'normalweight'
+    elif 23<BMI<=25:
+        res= 'overweight'
+    elif 25<BMI<=30:
+        res= 'obesity'
+    else:
+        res= 'an seriously obese'
+    return res
+print BMI()
