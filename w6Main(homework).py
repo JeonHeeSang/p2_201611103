7619e3cb014da56e7df959562692c61f1ec1c1e0
p2_201611103 전희상 <gmltkd1302@naver.com>
2016-04-07 23:58:47 +0900

    updowngame+윤년

---
+++ w6Main(homework).py
@@ -0,0 +1,42 @@
+﻿def leapyear():
+       year=input("User input year:")
+       if (year%4==0)and (year%100!=0 or year%400==0):
+       print "Leap year"
+       else:
+           print "Not Leap year"
+def updowngame():
+       import random
+       select=raw_input("input range number 1~100: ")
+       s1=int(select)
+       r1=random.randrange(1,s1+1)
+       print "range is 1~%f" % s1
+       def st():
+           selectr=raw_input("input number you want: ")
+           s3=int(select)
+           global s3
+       def mp():
+            if(s3==r1):
+                  print "nice"
+            else:
+                if(s3>r1):
+                   print "low"
+                   st()
+                   mp()
+            elif(s3<r1):
+                   print "high"
+                   st()
+                   mp()
+            else:
+                 print "error"
+    st()
+    mp()
+       
+
+def lab6():
+        leapyear()
+        updowngame()   
+def main():
+        lab6()
+
+if _name_=="_main_":
+       main()
