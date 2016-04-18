a6145c728f03971a59b384ceaf5a7cf7a37f2ecb
p2_201611103 전희상 <gmltkd1302@naver.com>
2016-04-18 16:50:04 +0900

    Update w7Main.py

--- w7Main.py
+++ w7Main.py
@@ -1,3 +1,6 @@
+import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
 ﻿def drawSquareAt(size, pos):
     t1.penup()
     t1.setpos(pos)
@@ -8,10 +11,27 @@
         t1.forward(size)
         t1.right(90)
     return tracks
-def lab7():
+import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+xtracks=input("input x pos:")
+ytracks=input("input y pos:")
+def drawSquareFrom(xtracks,ytracks):
+    x=((xtracks,ytracks),(xtracks+50,ytracks),(xtracks+50,ytracks+50),(xtracks,ytracks+50),(xtracks,ytracks))
+    t1.penup()
+    t1.goto(x[0])
+    t1.pendown()
+    for i in range(0,5):
+        t1.goto(x[i])        
+def lab7A():
     drawSquareAt(size,pos)
     print tracks
-def main():
-    lab7()
+def lab7B():
+    xtracks=input("input x pos:")
+    ytracks=input("input y pos:")
+    drawSquareFrom(xtracks,ytracks)
+def main:
+    lab7A()
+    lab7B()
 if_name_=="_main_":
      main()
