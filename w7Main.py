7ed151f20cdeace1952221ccb14b5da5fbfc688d
p2_201611103 전희상 <gmltkd1302@naver.com>
2016-04-18 15:54:31 +0900

    Added files via upload

---
+++ w7Main.py
@@ -0,0 +1,17 @@
+﻿def drawSquareAt(size, pos):
+    t1.penup()
+    t1.setpos(pos)
+    t1.pendown()
+    tracks=list()
+    for i in range(0,4):
+        tracks.append(t1.pos())
+        t1.forward(size)
+        t1.right(90)
+    return tracks
+def lab7():
+    drawSquareAt(size,pos)
+    print tracks
+def main():
+    lab7()
+if_name_=="_main_":
+     main()
