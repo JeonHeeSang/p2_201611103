781a4db74b02243504cbf0f9c06df578ec23c0f7
p2_201611103 전희상 <gmltkd1302@naver.com>
2016-04-20 13:50:27 +0900

    Update w7Main(위치저장).py

--- w7Main(위치저장).py
+++ w7Main(위치저장).py
@@ -1,63 +1,75 @@
-﻿import turtle
+import turtle
 wn=turtle.Screen()
 wn.bgpic("myMaze.gif")
 t1=turtle.Turtle()
 t1.speed(1)
-
 mytracks=list()
 
+def saveTracks():
+    t1.goto(-400,300)
 
-t1.goto(-400,300)
+    mytracks.append(t1.pos())
 
-mytracks.append(t1.pos())
+    t1.penup()
+    t1.pendown()
+    t1.pencolor("Red")
+    t1.right(90)
+    t1.fd(400)
 
-t1.penup()
-t1.pendown()
-t1.pencolor("Red")
-t1.right(90)
-t1.fd(400)
+    mytracks.append(t1.pos())
 
-mytracks.append(t1.pos())
+    t1.backward(150)
 
-t1.backward(150)
+    mytracks.append(t1.pos())
 
-mytracks.append(t1.pos())
+    t1.left(90)
+    t1.fd(300)
 
-t1.left(90)
-t1.fd(300)
+    mytracks.append(t1.pos())
 
-mytracks.append(t1.pos())
+    t1.left(90)
+    t1.fd(300)
 
-t1.left(90)
-t1.fd(300)
+    mytracks.append(t1.pos())
 
-mytracks.append(t1.pos())
+    t1.back(150)
 
-t1.back(150)
+    mytracks.append(t1.pos())
 
-mytracks.append(t1.pos())
+    t1.right(90)
+    t1.fd(300)
 
-t1.right(90)
-t1.fd(300)
+    mytracks.append(t1.pos())
 
-mytracks.append(t1.pos())
+    t1.left(90)
+    t1.right(90)
+    t1.right(90)
+    t1.fd(200)
 
-t1.left(90)
-t1.right(90)
-t1.right(90)
-t1.fd(200)
+    mytracks.append(t1.pos())
 
-mytracks.append(t1.pos())
+    t1.fd(300)
 
-t1.fd(300)
+    mytracks.append(t1.pos())
 
-mytracks.append(t1.pos())
+    t1.back(100)
 
-t1.back(100)
+    mytracks.append(t1.pos())
 
-mytracks.append(t1.pos())
+    t1.left(90)
+    t1.fd(200)
 
-t1.left(90)
-t1.fd(200)
+    mytracks.append(t1.pos())
+    return mytracks
 
-mytracks.append(t1.pos())
+
+def replayTracks(mytracks):
+    for i in mytracks:
+        print i
+        
+def lab7():
+    mytracks=saveTracks()
+    replayTracks(mytracks)
+    
+def main():
+    lab7()
