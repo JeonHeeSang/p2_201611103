c4b1d28ab59b95e59b7f3263d164bef05367ac32
p2_201611103 전희상 <gmltkd1302@naver.com>
2016-05-02 17:22:53 +0900

    Added files via upload

---
+++ w9Main.py
@@ -0,0 +1,69 @@
+﻿import matplotlib
+import matplotlib.pyplot as plt
+def charCount():
+    d=dict()
+    sentence=raw_input("input word :")
+    for c in sentence:
+        if c not in d:
+            d[c]=1
+        else:
+            d[c]+=1
+
+    plt.bar(range(len(d)),d.values(), align='center')
+    plt.xticks(range(len(d)), list(d.keys()))
+    plt.show()
+
+def countDigitsLetter(word):
+    d=dict()
+    d={"number":0, "word":0}
+    for i in word:
+        if i.isdigit()==True:
+            d["number"]=d["number"]+1
+        elif i.isdigit()==False:
+            d["word"]=d["word"]+1
+    import matplotlib
+    import matplotlib.pyplot as plt
+
+    plt.bar(range(len(d)),d.values(), align='center')
+    plt.xticks(range(len(d)), list(d.keys()))
+    plt.show()
+
+myhome=set()
+yourhome=set()
+myhome={'TV', 'phone', 'camera', 'fridger', 'mixer', 'audio', 'cd player', 'light', 'computer', 'notebook', 'recorder'}
+yourhome={'coffee machine', 'radio', 'camera', 'running machine', 'ramp', 'computer', 'notebook ', 'recorder'}
+def Mine():
+    print myhome.difference(yourhome)
+def Yours():
+    print yourhome.difference(myhome)
+def Both():
+    print myhome.intersection(yourhome)
+def All():
+    print myhome.union(yourhome)
+def Count():
+    d=dict()
+    for i in myhome:
+        if i not in d:
+            d[i]=1
+        else:
+            d[i]+=1
+    for i in yourhome:
+        if i not in d:
+            d[i]=1
+        else:
+            d[i]+=1
+    print d        
+def lab9():
+    charCount()
+    word=raw_input("input word")
+    countDigitsLetter(word)
+    Mine()
+    Yours()
+    Both()
+    All()
+    Count()
+def main():
+    lab9()
+
+main()
+raw_input("zzzzz:")
