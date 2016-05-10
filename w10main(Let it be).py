766217618eb0a24ee4260574cb2fd8a59d128aa9
p2_201611103 전희상 <gmltkd1302@naver.com>
2016-05-11 01:38:52 +0900

    Add files via upload

---
+++ w10main(Let it be).py
@@ -0,0 +1,54 @@
+﻿song=list()
+song=["When I find myself in times of trouble",
+    "Mother Mary comes to me",
+    "Speaking words of wisdom let it be",
+    "And in my hour of darkness",
+    "She is standing right in front of me",
+    "Speaking words of wisdom let it be",
+
+    "Let it be let it be",
+    "Let it be let it be",
+    "Whisper words of wisdom let it be",
+
+    "And when the broken-hearted people",
+    "Living in the world agree",
+    "There will be an answer let it be",
+    "For though they may be parted",
+    "There is still a chance that they will see",
+    "There will be an answer let it be",
+
+    "Let it be let it be",
+    "Let it be let it be",
+    "Yeah there will be an answer let it be",
+    "Let it be let it be",
+    "Let it be let it be",
+    "Whisper words of wisdom let it be",
+
+    "Let it be let it be",
+    "Ah let it be yeah let it be",
+    "Whisper words of wisdom let it be",
+
+    "And when the night is cloudy",
+    "There is still a light that shines on me",
+    "Shine on until tomorrow let it be",
+    "I wake up to the sound of music",
+    "Mother Mary comes to me",
+    "Speaking words of wisdom let it be",
+
+    "Let it be let it be",
+    "Let it be yeah let it be",
+    "Oh there will be an answer let it be",
+    "Let it be let it be",
+    "Let it be yeah let it be",
+    "Whisper words of wisdom let it be"]
+doc=song
+d=dict()
+for line in doc:
+    words=line.split()
+    for word in words:
+        if word not in d:
+            d[word]=1
+        else:
+            d[word]+=1
+print max(d, key=d.get)
+input()
