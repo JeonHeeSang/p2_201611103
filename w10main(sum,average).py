62be46ad9d578f32dd459977237ff1caccd2ace0
p2_201611103 전희상 <gmltkd1302@naver.com>
2016-05-11 01:36:52 +0900

    Add files via upload

---
+++ w10main(sum,average).py
@@ -0,0 +1,26 @@
+﻿Mylist=list()
+Mylist=[["English", 100],
+       ["Math", 200],
+       ["English", 200],
+       ["Math", 200],
+       ["English", 100],
+       ["Math", 300]]
+Esum=0
+Msum=0
+en=0
+ma=0
+for i in Mylist:
+    if i[0]=="English":
+        en+=1
+    elif i[0]=="Math":
+        ma+=1
+for i in Mylist:
+    if i[0]=="English":
+        Esum+=i[1]
+    elif i[0]=="Math":
+        Msum+=i[1]        
+print  "English Total:", Esum        
+print "Math Total:", Msum
+print "English average:", Esum/en
+print "Math average:", Msum/ma
+input()
