958503000f35ee9e609fbdb256f03ba562a3b188
p2_201611103 전희상 <gmltkd1302@naver.com>
2016-05-11 01:36:37 +0900

    Add files via upload

---
+++ w10main(Milk).py
@@ -0,0 +1,14 @@
+﻿mylist=list()
+mylist=[["Coffee","Water","Milk","Icecream"],
+        ["Espresso","No","No","No"],
+        ["Long Black","Yes","No","No"],
+        ["Flat White","No","Yes","No"],
+        ["Cappuccino","No","Yes - Frothy","No"],
+        ["Affogato","No","No","Yes"]]
+data=mylist[1:] 
+sum=0
+for i in data:
+    if i[2].upper()=='YES':
+        sum=sum+1
+print "percent:", float(sum)/float(len(data))
+input()
