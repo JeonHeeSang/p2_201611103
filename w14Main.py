a1fa70cecff8788f9697eb4dcf9ea54d0a9ebc00
p2_201611103 전희상 <gmltkd1302@naver.com>
2016-06-02 22:48:15 +0900

    Add files via upload

---
+++ w14Main.py
@@ -0,0 +1,29 @@
+﻿class Dog(object):
+    def __init__(self,name):
+        self.name=name
+    def getname(self):
+        print "my dog name is",self.name
+    def talk(self):
+        print "mung mung"
+class ShihTzuDog(Dog):
+    def talk(self):
+        print "si si"
+class Maltese(Dog):
+    def talk(self):
+        print "wal wal"
+def dogsound():
+    mydog=Dog('poppy')
+    mydog.talk()
+    myshitzu=ShiTzuDog('poppy')
+    myshitzu.talk()
+    mymaltese=MalteseDog('poppy')
+    mymaltese.talk()
+    
+def lab14():
+    dogsound()
+    
+def main():   
+    lab14()
+
+if __name__=="__main__":
+    main()
